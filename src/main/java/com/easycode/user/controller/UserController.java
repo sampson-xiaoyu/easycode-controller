3b8908b5782636b26804f016ae5243c30a5909f2
xiaoyu <xiaoyu@DESKTOP-A2NK211>
2016-11-17 01:29:36 +0800

    更新

--- src/main/java/com/easycode/user/controller/UserController.java
+++ src/main/java/com/easycode/user/controller/UserController.java
@@ -1,8 +1,18 @@
 package com.easycode.user.controller;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
+
+import com.easycode.client.SearchClient;
 
 @Controller
 public class UserController {
-
+	
+	@Resource(name = "searchClient")
+	private SearchClient searchClient;
+	
+	public void queryUsers(){
+		
+	}
 }
