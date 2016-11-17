642a6178f3c8397efd34061b73e38af360eec9b3
xiaoyu <xiaoyu@192.168.116.1>
2016-11-18 00:59:41 +0800

    更新

--- src/main/java/com/easycode/user/web/UserController.java
+++ src/main/java/com/easycode/user/web/UserController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import com.easycode.common.baseVO.BaseVO;
 import com.easycode.common.baseVO.Code;
 import com.easycode.web.interceptors.UserLoginAnnotation;
@@ -12,13 +14,14 @@ import com.easycode.web.interceptors.UserLoginAnnotation;
 public class UserController {
 	
 	@UserLoginAnnotation
+	@ResponseBody
 	@RequestMapping(value="/login" , method = {RequestMethod.POST , RequestMethod.GET})
 	public BaseVO userLogin(@RequestParam(value="username",required = true) String userName,
 			@RequestParam(value = "password",required = true) String password){
 		
 		BaseVO baseVO = new BaseVO();
 		
-		if("xiaoyu".equals(userName) && "123456".equals(password)){
+		if(!"xiaoyu".equals(userName) || !"123456".equals(password)){
 			baseVO.setCode(Code.FAIL.text());
 			baseVO.setCode(Code.FAIL.text());
 		}
