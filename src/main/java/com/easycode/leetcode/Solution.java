bfec4238480add9fee44f4a04006804fdf054372
qi.yu <qi.yu@bianlifeng.com>
2019-04-03 00:44:44 +0800

    save

--- src/main/java/com/easycode/leetcode/Solution.java
+++ src/main/java/com/easycode/leetcode/Solution.java
@@ -7,69 +7,112 @@ import java.util.Map;
 public class Solution {
 
     public static void main(String arg[]) {
-        int[] arrays1 = twoNumsum1(new int[]{3,3}, 6);
+        //int[] arrays1 = twoNumsum1(new int[]{3,3}, 6);
         /*Arrays.asList(arrays).forEach(x -> {
             System.out.println(x);
         });*/
 
-        for (int i : arrays1) {
+        /*for (int i : arrays1) {
             System.out.print(i);
             System.out.print(" ");
-        }
-        ;
+        }*/
+
+        System.out.println(lengthOfLongestSubstring("dvdf"));
+
+
     }
 
-    public static int[] twoNumSum(int[] nums, int target) {
-
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
+    public int[] twoSum1(int[] nums, int target) {
+        for(int i = 0; i < nums.length - 1; i++){
+            for(int j= i + 1; j < nums.length ; j++){
+                if(nums[i] + nums[j] == target){
+                    return new int[]{i,j};
                 }
             }
         }
         return new int[]{};
     }
-
-    public static int[] twoNumsum1(int[] nums, int target) {
-
-        Map<String, Integer> result = new HashMap<String, Integer>();
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
         for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] { i, map.get(complement) };
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
 
-            String key1 = ""+(target - nums[i]);
-            if( nums[i] != target - nums[i]) {
-                result.put(key1, i);
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode h1 = l1;
+        ListNode h2 = l2;
+        ListNode r = null;
+        ListNode rh = null;
+        int jin = 0;
+        while (h1 != null || h2 != null){
+
+
+            int x = h1 == null ? 0 : h1.val;
+            int y = h2 == null ? 0 : h2.val;
+            int sum = x + y + jin;
+            jin = sum / 10;
+            if(jin > 0){
+                sum = sum%10;
+            }
+            if(r == null){
+                rh = r = new ListNode(sum);
             }else {
-                if(!result.containsKey(key1)){
-                    result.put(key1, i);
-                }else {
-                    result.put("a"+key1, i);
-                }
+                r.next = new ListNode(sum);
+                r = r.next;
+            }
 
+            if(h1 != null) {
+                h1 = h1.next;
+            }
+            if(h2 != null) {
+                h2 = h2.next;
             }
         }
 
-        for (int i = 0; i < nums.length; i++) {
+        if(jin > 0){
+            r.next = new ListNode(jin);
+        }
+        return rh;
+    }
 
-            if(nums[i] != target - nums[i]) {
-                String key1 = (target - nums[i]) + "";
-                String key2 = nums[i] + "";
-                if (result.containsKey(key1)
-                        && result.containsKey(key2)) {
-                    return new int[]{result.get(key1), result.get(key2)};
-                }
-            }else {
-                String key1 = (target - nums[i]) + "";
-                String key2 = "a"+key1;
-                if (result.containsKey(key1)
-                        && result.containsKey(key2)) {
-                    return new int[]{result.get(key1), result.get(key2)};
-                }
-            }
+    public static class ListNode{
+
+        int val;
+
+        ListNode next;
+
+        ListNode(int val){
+            this.val = val;
         }
 
-        return new int[]{};
+    }
 
+    public static int lengthOfLongestSubstring(String s){
+
+        int maxLength = 0;
+
+        String temp = "";
+        for(int i =0 ;i < s.length(); i++){
+
+            String t = String.valueOf(s.charAt(i));
+            if(!temp.contains(t)){
+                temp = temp + t;
+                maxLength = Math.max(temp.length(),maxLength);
+            }else {
+                temp = temp.substring(temp.lastIndexOf(t)+1,temp.length()) + t;
+            }
+
+        }
+        return maxLength;
     }
 
 }
